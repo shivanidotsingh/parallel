1ceeb45eaf57b869581be147e8eac6484e20714d
s <60198743+shivanidotsingh@users.noreply.github.com>
2025-04-24 20:28:41 +0200

    Update script.js

--- script.js
+++ script.js
@@ -13,6 +13,13 @@ function showEvents() {
     event1.textContent = "No data available for this year.";
     event2.textContent = "";
   }
+
+  const color = colorCombos[Math.floor(Math.random() * colorCombos.length)];
+  document.body.style.backgroundColor = color.bg;
+  document.body.style.color = color.text;
+
+  const button = document.querySelector("button");
+  button.style.color = color.text;
 }
 
 function shuffle(array) {
@@ -29,17 +36,19 @@ function shuffle(array) {
   return arrayCopy;
 }
 
-function randomYear() {
-  // Get all available years from the eventsByYear object
-  const availableYears = Object.keys(eventsByYear);
-  
-  // Select a random year from the available years
-  const randomIndex = Math.floor(Math.random() * availableYears.length);
-  const randomYear = availableYears[randomIndex];
-  
-  // Update the input field with the random year
-  document.getElementById('yearInput').value = randomYear;
-  
-  // Show the events for this random year
-  showEvents();
-}
+const colorCombos = [
+
+  { bg: "#CBA0AA", text: "#FAE397" },
+  { bg: "#B6CAC0", text: "#C02A1B" },  
+  { bg: "#F9F7E8", text: "#62BFAD" },
+  { bg: "#6C5B7B", text: "#C06C84" },
+  { bg: "#355C7D", text: "#F67280" },
+  { bg: "#F3C9DD", text: "#72AEC5" },
+  { bg: "#119DA4", text: "#FFC857" },
+  { bg: "#20AD65", text: "#FEC8BE" },
+  { bg: "#9C9CDD", text: "#CAE9BF" },
+  { bg: "#B2B2B2", text: "#E9FF27" },
+  { bg: "#8AA9C6", text: "#D1BDFF" },
+  { bg: "#393E41", text: "#E94F37" }
+];
+
