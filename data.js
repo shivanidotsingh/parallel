85de1e97683d6cd24ebbcf0ac2a93f0fe1330250
s <60198743+shivanidotsingh@users.noreply.github.com>
2025-04-24 18:13:41 +0200

    Create data.js

---
+++ data.js
@@ -0,0 +1,35 @@
+
+const eventsByYear = {
+  "1840": [
+    "The World Anti-Slavery Convention convened in London.",
+    "The Treaty of Waitangi was signed in New Zealand."
+  ],
+  "1841": [
+    "President William Henry Harrison died after one month in office.",
+    "The Supreme Court ruled in favor of the Amistad captives."
+  ],
+  "1842": [
+    "Webster-Ashburton Treaty resolved the US-Canada border dispute.",
+    "Treaty of Nanking ended the First Opium War, ceding Hong Kong to Britain."
+  ],
+  "1843": [
+    "First major wagon train set off on the Oregon Trail.",
+    "Charles Dickens published 'A Christmas Carol'."
+  ],
+  "1844": [
+    "First telegraph message sent by Samuel Morse.",
+    "Dominican Republic declared independence from Haiti."
+  ],
+  "1845": [
+    "United States annexed Texas.",
+    "Great Famine began in Ireland."
+  ],
+  "1846": [
+    "Mexican-American War began.",
+    "Oregon Treaty established US-Canada border."
+  ],
+  "1847": [
+    "Battle of Buena Vista in the Mexican-American War.",
+    "The Brontë sisters published Jane Eyre and Wuthering Heights."
+  ]
+};
